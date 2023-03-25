b46594e9a2e90d5aabafa53fa71df9cd3a6b75e2
HinoShun <shumpei.hino88@icloud.com>
2023-03-25 18:51:02 +0900

    画像投稿実装

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,11 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :tweet, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+
 end
