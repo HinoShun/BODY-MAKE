e09bb5b81adb4f410c34354999cc78e20266af78
HinoShun <shumpei.hino88@icloud.com>
2023-03-25 18:14:34 +0900

    投稿機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,25 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
   end
   
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:image, :tweet,).merge(user_id: current_user.id)
+  end
+
 end
