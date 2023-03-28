2b94d375362f58b9e8985fcada87986549954c53
HinoShun <shumpei.hino88@icloud.com>
2023-03-29 02:13:54 +0900

    new作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,35 @@
 class UsersController < ApplicationController
+  before_action :set_user,            only: [:show, :edit, :update]
+
   def show
     @user = User.find(params[:id])
     #@tweets = user.tweets
   end
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(user.id)
+    else
+      render action: :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:image, :introduction, :status_id, :target_date, :target_weight, :target_fat)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
 end
