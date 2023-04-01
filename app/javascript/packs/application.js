d64d976bc90faa9bd62f486055e21da044d72906
HinoShun <shumpei.hino88@icloud.com>
2023-04-01 21:16:00 +0900

    グラフ表示実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -16,6 +16,4 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-//= require highcharts/highcharts
-//= require highcharts/highcharts-more
-//= require highcharts/highstock
+import "chartkick/chart.js"
