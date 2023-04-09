af64cdd04e61b42beb70f4a31465287620eab695
HinoShun <shumpei.hino88@icloud.com>
2023-04-09 15:54:33 +0900

    コメント機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
+  before_action :set_tweet,          only: [:new, :create, :destroy]
+
+  def new
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:tweet_id])
+  end
 end
