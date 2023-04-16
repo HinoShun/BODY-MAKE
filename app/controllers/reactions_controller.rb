be1e9cbb81a9bf58dcdc5cd02ffb47172c0ed5c1
HinoShun <shumpei.hino88@icloud.com>
2023-04-16 18:44:22 +0900

    リアクション関連　コメント追記

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -4,12 +4,14 @@ class ReactionsController < ApplicationController
   def create	
     like = current_user.reactions.build(tweet_id: params[:tweet_id])	
     like.save	
+    # データ保存後、部分表示
     render 'create.js.erb'
   end	
 	
   def destroy	
     like = Reaction.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)	
     like.destroy	
+    # データ削除後、部分表示
     render 'destroy.js.erb'
   end	
 
